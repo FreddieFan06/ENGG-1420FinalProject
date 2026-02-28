110267429120a213eceff81b23d042ed9af1fdf0
FreddieFan06 <151697563+FreddieFan06@users.noreply.github.com>
2026-02-28 05:26:18 -0500

    Some junk code to test if JavaFX worked with VSCode.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,26 @@
 package main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("ENGG-1420 Final Project Beginning!");
+        btn.setOnAction(e -> System.out.println("JavaFX is alive!"));
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        primaryStage.setScene(new Scene(root, 400, 250));
+        primaryStage.setTitle("Campus Event System");
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("ENGG-1420 Final Project Beginning");
+        launch(args);
     }
 }
