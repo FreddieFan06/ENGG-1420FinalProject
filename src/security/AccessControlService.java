26c8493d8d05549866316e98fe45ba6b3cfe374c
Facepunch-345 <perlmutare@gmail.com>
2026-03-10 17:32:15 -0400

    Created boolean method that checks if the user is allowed to override capacity limits (i.e. checks if they are STAFF)

--- src/security/AccessControlService.java
+++ src/security/AccessControlService.java
@@ -1,10 +1,12 @@
 package security;
 
 import model.enums.UserType;
+import model.users.User;
 
 public class AccessControlService {
-/*  public AccessControlService() {
-        if (UserType == STUDENT) {
+    public AccessControlService() {
+
+        /*if (UserType == STUDENT) {
             Set access to 'protected'
             Should only allow access to events
             from the user.
@@ -15,9 +17,18 @@ public class AccessControlService {
             Should allow access to all events.
         }
         
-        if (UserType == STUDENT) {
+        if (UserType == GUEST) {
             Set access to 'private';
             shouldn't allow any edit access.
+        }*/
+    }
+
+    public boolean canOverrideUserBookingLimit(User user) {
+        if (user.getUserType() == UserType.STAFF) {
+            return true;
         }
-    }*/
+        else {
+            return false;
+        }
+    }
 }
