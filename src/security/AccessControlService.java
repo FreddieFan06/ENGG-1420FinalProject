4ef372b1a613e0309eea24aeae0b323c8470e026
Facepunch-345 <perlmutare@gmail.com>
2026-03-09 23:58:49 -0400

    Created outline for AccessControlService functions

--- src/security/AccessControlService.java
+++ src/security/AccessControlService.java
@@ -3,4 +3,21 @@ package security;
 import model.enums.UserType;
 
 public class AccessControlService {
+/*  public AccessControlService() {
+        if (UserType == STUDENT) {
+            Set access to 'protected'
+            Should only allow access to events
+            from the user.
+        } 
+        
+        if (UserType == STAFF) {
+            Set access to 'public';
+            Should allow access to all events.
+        }
+        
+        if (UserType == STUDENT) {
+            Set access to 'private';
+            shouldn't allow any edit access.
+        }
+    }*/
 }
